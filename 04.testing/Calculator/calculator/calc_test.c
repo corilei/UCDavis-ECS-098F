a9b1c30938a850b3a72a062e6f768a0cb4246d9e
corilei <990941570@qq.com>
2022-10-18 17:40:33 +0800

    1

--- 04.testing/Calculator/calculator/calc_test.c
+++ 04.testing/Calculator/calculator/calc_test.c
@@ -1,22 +1,28 @@
-/*
- * Program to test the calculator program 
- */
-#include <stdio.h>
-#include <stdbool.h>
+  {.expression="28+1", .result=29},
+  {.expression="38+1", .result=39},
 
-#include "calc.h"
-
-#define NUM_TESTS 1
-
-struct pair{
-  char *expression;
-  int result;
-};
-
-struct pair input_output_pairs[NUM_TESTS] = {
-  // correct calculation
-  {.expression="1+1", .result=1}
-  // incorrect calculation
+  {.expression="17+1", .result=18},
+  {.expression="16+1", .result=17},
+  {.expression="15+1", .result=16},
+  {.expression="14+1", .result=15},
+  {.expression="13+1", .result=14},
+  {.expression="12+1", .result=13},
+  {.expression="11+1", .result=12},
+  {.expression="10+1", .result=11},
+  // -
+  {.expression="1-2", .result=-1},
+  // *
+  {.expression="0*1", .result=0},
+  {.expression="99*100", .result=9900},
+  // /
+  {.expression="3/2", .result=1},
+  {.expression="9/3", .result=3},
+  // ()
+  {.expression="(0-3)/2", .result=-1},
+  {.expression="3/(2-4)", .result=-1},
+  // combine
+  {.expression="3/(2-4)+2", .result=1},
+  {.expression="3+1*2", .result=5}
 };
 
 // Given the index of an input-output pair, run the test on the username
@@ -26,8 +32,6 @@ bool run_test(int test_num) {
   int real_output = calc(input);
   int expected_output = input_output_pairs[test_num].result;
     if(real_output != expected_output) {
-      printf("Test %i failed! Expected %s to %sbe a valid username\n", test_num + 1,
-            input, expected_output ? "" : "not ");
       printf("Test %i failed! Expected %s = %i , not %i\n", test_num + 1,
             input, expected_output, real_output);
       return false;
@@ -44,3 +48,4 @@ int main(int argc, char **argv) {
   }
   printf("%i out of %i tests passed.\n", num_passed, NUM_TESTS);
 }
+                                                                                                                           82,1          Bot
