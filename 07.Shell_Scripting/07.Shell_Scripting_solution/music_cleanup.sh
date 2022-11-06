7ca61677e98310cf666f7dfe045c73c64857a514
corilei <990941570@qq.com>
2022-11-07 03:22:42 +0800

    1

--- 07.Shell_Scripting/07.Shell_Scripting_solution/music_cleanup.sh
+++ 07.Shell_Scripting/07.Shell_Scripting_solution/music_cleanup.sh
@@ -8,26 +8,27 @@ getArtistName() {
     # I wrote this one to take one argument and set a global variable to the
     # artist's name with whitespace removed
     # Note that this is where you'll want/need to use ffprobe
-    output=$(ffprobe -show_entries format_tags=artist -of csv=p=0 -loglevel quiet ${current_file})
+    output=$(ffprobe -show_entries format_tags=artist -of csv=p=0 -loglevel quiet "${current_file}")
 
     artistName=${output//" "/""}
 
-    echo "artist: ${artistName}"
+#    echo "artist: ${artistName}"
 }
 
 getSongName() {
     # Same deal as getArtistName but with the song name
     # Note that this is where you'll want/need to use ffprobe
-    output=$(ffprobe -show_entries format_tags=title -of csv=p=0 -loglevel quiet ${current_file})
+    output=$(ffprobe -show_entries format_tags=title -of csv=p=0 -loglevel quiet "${current_file}")
 
     songName=${output//" "/""}
+    songName=${songName//"\""/""}
 
-    echo "title: ${songName}"
+#    echo "title: ${songName}"
 }
 
 doConversion() {
     # This function performs the conversion on a single file
-    ffmpeg -loglevel error -i ${current_file} ${TARGET_DIR}/${artistName}_${songName}.mp3
+    ffmpeg -loglevel error -i "${current_file}" "${TARGET_DIR}"/"${artistName}"_"${songName}".mp3
 }
 
 # Takes one argument, that being a directory
@@ -35,15 +36,15 @@ TARGET_DIR=$1
 
 # Now we should iterate over every file in the directory and do the conversion
 # for each file
-for file in ${TARGET_DIR}/*
+for file in "${TARGET_DIR}"/*
 do
   current_file=${file}
 
   getArtistName
   getSongName
   doConversion
-  rm ${current_file}
-  echo "remove ${current_file}"
+  rm "${current_file}"
+#  echo "remove ${current_file}"
 done
 
 exit 0
